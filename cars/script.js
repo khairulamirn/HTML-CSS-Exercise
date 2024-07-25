778e625b1a8b82b18916ea4e30be3c30a8192cf1
TheKiruls <amirin856@gmail.com>
2024-07-25 23:00:08 +0800

    continue with z-index

--- cars/script.js
+++ cars/script.js
@@ -1,5 +1,12 @@
 const lis = document.querySelectorAll('.li');
 
+/**
+ * Adds event listeners to each list item to show and hide a horizontal rule element
+ * when the mouse is over the list item.
+ * @param {NodeListOf<HTMLLIElement>} lis - The list items to add event listeners to.
+ * @returns None
+ */
+
 lis.forEach((li) => {
     const hr = li.querySelector('.hr');
     
@@ -11,3 +18,20 @@ lis.forEach((li) => {
         hr.classList.remove('active');
     });
 });
+
+// const lis = document.querySelectorAll('.li');
+
+// const handleMouseOver = (event) => {
+//     const hr = event.currentTarget.querySelector('.hr');
+//     hr.classList.add('active');
+// };
+
+// const handleMouseOut = (event) => {
+//     const hr = event.currentTarget.querySelector('.hr');
+//     hr.classList.remove('active');
+// };
+
+// lis.forEach((li) => {
+//     li.addEventListener('mouseover', handleMouseOver);
+//     li.addEventListener('mouseout', handleMouseOut);
+// });
