5cb52fe736be0d970aab4fff2e40ec7cebebd1d3
TheKiruls <amirin856@gmail.com>
2024-07-29 23:40:15 +0800

    done img script

--- cars/script.js
+++ cars/script.js
@@ -35,3 +35,43 @@ lis.forEach((li) => {
 //     li.addEventListener('mouseover', handleMouseOver);
 //     li.addEventListener('mouseout', handleMouseOut);
 // });
+
+// Division and modulo are two different operations. The division operator gives you the quotient after dividing two integers, ignoring the remainder. On the other hand, the modulo operator gives you the remainder after dividing two integers, ignoring the quotient.
+
+const carImg = document.querySelectorAll('.car');
+const carImgArray = Array.from(carImg);
+console.log(carImgArray);
+
+const buttonImgLeft = document.querySelector('.btn1');
+const buttonImgRight = document.querySelector('.btn2');
+
+// Ensure first image is active
+let currentImg = 0;
+console.log(currentImg);
+carImgArray[currentImg].classList.add('active');
+
+buttonImgRight.addEventListener('click', () => {
+    // Remove the active class from the current image
+    carImgArray[currentImg].classList.remove('active');
+    
+    // Update the current image index
+    currentImg = (currentImg + 1) % carImgArray.length;
+    
+    // Add the active class to the new image
+    carImgArray[currentImg].classList.add('active');
+
+    console.log('Right button clicked, currentImg:', currentImg);
+});
+
+buttonImgLeft.addEventListener('click', () => {
+    // Remove the active class from the current image
+    carImgArray[currentImg].classList.remove('active');
+    
+    // Update the current image index
+    currentImg = (currentImg - 1 + carImgArray.length) % carImgArray.length;
+    
+    // Add the active class to the new current image
+    carImgArray[currentImg].classList.add('active');
+
+    console.log('Left button clicked, currentImg:', currentImg);
+});
